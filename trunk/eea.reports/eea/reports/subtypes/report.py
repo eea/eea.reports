659c13feb033c30a21fe92313068f09169dcbd24
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2008-07-31 15:31:00 +0000

    - #1722 - Define custom macro for keywords widget and use   it with creators, publishers fields.

--- trunk/eea.reports/eea/reports/subtypes/report.py
+++ trunk/eea.reports/eea/reports/subtypes/report.py
@@ -11,49 +11,19 @@ from slc.publications.subtypes.publication import ExtensionFieldMixin
 from eea.reports.config import COPYRIGHTS
 from eea.reports.vocabulary import ReportYearsVocabulary, ReportThemesVocabulary
 
-class ReportTypeFiled(ExtensionField, ExtensionFieldMixin, atapi.StringField):
+class ReportStringField(ExtensionField, ExtensionFieldMixin, atapi.StringField):
     """ """
 
-class ReportNumField(ExtensionField, ExtensionFieldMixin, atapi.StringField):
+class ReportIntegerField(ExtensionField, ExtensionFieldMixin, atapi.IntegerField):
     """ """
 
-class SeriesYearField(ExtensionField, ExtensionFieldMixin, atapi.StringField):
+class ReportLinesField(ExtensionField, ExtensionFieldMixin, atapi.LinesField):
     """ """
 
-class SeriesTitleField(ExtensionField, ExtensionFieldMixin, atapi.StringField):
+class ReportFloatField(ExtensionField, ExtensionFieldMixin, atapi.FloatField):
     """ """
 
-class TrailerField(ExtensionField, ExtensionFieldMixin, atapi.StringField):
-    """ """
-
-class CreatorsOrgsField(ExtensionField, ExtensionFieldMixin, atapi.StringField):
-    """ """
-
-class CreatorsField(ExtensionField, ExtensionFieldMixin, atapi.StringField):
-    """ """
-
-class PublishersOrgsField(ExtensionField, ExtensionFieldMixin, atapi.StringField):
-    """ """
-
-class PublishersField(ExtensionField, ExtensionFieldMixin, atapi.StringField):
-    """ """
-
-class ThemesField(ExtensionField, ExtensionFieldMixin, atapi.StringField):
-    """ """
-
-class PriceField(ExtensionField, ExtensionFieldMixin, atapi.StringField):
-    """ """
-
-class OrderOverrideTextField(ExtensionField, ExtensionFieldMixin, atapi.StringField):
-    """ """
-
-class OrderExtraTextField(ExtensionField, ExtensionFieldMixin, atapi.StringField):
-    """ """
-
-class CopyrightField(ExtensionField, ExtensionFieldMixin, atapi.StringField):
-    """ """
-
-class PagesField(ExtensionField, ExtensionFieldMixin, atapi.StringField):
+class ReportTextField(ExtensionField, ExtensionFieldMixin, atapi.TextField):
     """ """
 
 class SchemaExtender(PublicationSchemaExtender):
@@ -61,7 +31,7 @@ class SchemaExtender(PublicationSchemaExtender):
     """
     implements(IOrderableSchemaExtender)
     _fields = PublicationSchemaExtender._fields + [
-            ReportTypeFiled('reporttype',
+            ReportStringField('reporttype',
                 schemata='report',
                 languageIndependent=False,
                 default=u'',
@@ -71,7 +41,7 @@ class SchemaExtender(PublicationSchemaExtender):
                     description=_(u'description_reporttype', default=u'Fill in report-type'),
                 ),
             ),
-            ReportNumField('reportnum',
+            ReportIntegerField('reportnum',
                 schemata='report',
                 languageIndependent=False,
                 default=1,
@@ -80,7 +50,7 @@ class SchemaExtender(PublicationSchemaExtender):
                     description=_(u'description_reportnum', default=u'Fill in report-number'),
                 ),
             ),
-            SeriesYearField('series_year',
+            ReportIntegerField('series_year',
                 schemata='report',
                 languageIndependent=False,
                 default=u'',
@@ -90,7 +60,7 @@ class SchemaExtender(PublicationSchemaExtender):
                     description=_(u'description_series_year', default=u'Fill in report-year'),
                 ),
             ),
-            SeriesTitleField('series_title',
+            ReportStringField('series_title',
                 schemata='report',
                 languageIndependent=True,
                 widget=atapi.StringWidget(
@@ -98,41 +68,29 @@ class SchemaExtender(PublicationSchemaExtender):
                     description=_(u'description_reporttype', default=u'Fill in report-type'),
                 ),
             ),
-            CreatorsOrgsField('creators_orgs',
+            ReportLinesField('creators',
                 schemata='report',
-                languageIndependent=False,
+                languageIndependent=True,
+                multiValued=1,
                 vocabulary=NamedVocabulary("report_creators"),
-                widget=atapi.MultiSelectionWidget(
-                    label=_(u'label_creators_orgs', default=u'Authors (organisations)'),
-                    description=_(u'description_creators_orgs', default=u'Fill in authors(organisations)'),
-                ),
-            ),
-            CreatorsField('creators',
-                schemata='report',
-                languageIndependent=True,
-                widget=atapi.LinesWidget(
-                    label=_(u'label_creators', default=u'Additional Creators/Authors'),
+                widget=atapi.KeywordWidget(
+                    label=_(u'label_creators', default=u'Creators/Authors'),
                     description=_(u'description_creators', default=u'Fill in additional creators/authors'),
+                    macro='report_keywords',
                 ),
             ),
-            PublishersOrgsField('publishers_orgs',
-                schemata='report',
-                languageIndependent=False,
-                vocabulary=NamedVocabulary("report_publishers"),
-                widget=atapi.MultiSelectionWidget(
-                    label=_(u'label_publishers_orgs', default=u'Publishers'),
-                    description=_(u'description_publishers_orgs', default=u'Fill in publishers'),
-                ),
-            ),
-            PublishersField('publishers',
+            ReportLinesField('publishers',
                 schemata='report',
                 languageIndependent=True,
-                widget=atapi.LinesWidget(
-                    label=_(u'label_publishers', default=u'Additional Publishers'),
+                multiValued=1,
+                vocabulary=NamedVocabulary("report_publishers"),
+                widget=atapi.KeywordWidget(
+                    label=_(u'label_publishers', default=u'Publishers'),
                     description=_(u'description_publishers', default=u'Fill in additional publishers'),
+                    macro='report_keywords',
                 ),
             ),
-            ThemesField('themes',
+            ReportLinesField('themes',
                 schemata='report',
                 validators=('maxValues',),
                 vocabulary=ReportThemesVocabulary(),
@@ -146,7 +104,7 @@ class SchemaExtender(PublicationSchemaExtender):
                 index="KeywordIndex:brains",
                 enforceVocabulary=1
             ),
-            PriceField('price',
+            ReportFloatField('price',
                 schemata='report',
                 languageIndependent=False,
                 widget=atapi.DecimalWidget(
@@ -154,7 +112,7 @@ class SchemaExtender(PublicationSchemaExtender):
                     description=_(u'description_price', default=u'Fill in price'),
                 ),
             ),
-            OrderOverrideTextField('order_override_text',
+            ReportTextField('order_override_text',
                 schemata='report',
                 languageIndependent=True,
                 widget=atapi.RichWidget(
@@ -162,7 +120,7 @@ class SchemaExtender(PublicationSchemaExtender):
                     description=_(u'description_order_override_text', default=u'Fill in to override the order text'),
                 ),
             ),
-            OrderExtraTextField('order_extra_text',
+            ReportTextField('order_extra_text',
                 schemata='report',
                 languageIndependent=True,
                 widget=atapi.RichWidget(
@@ -170,7 +128,7 @@ class SchemaExtender(PublicationSchemaExtender):
                     description=_(u'description_order_extra_text', default=u'Fill in to add this text to the order text'),
                 ),
             ),
-            PagesField('pages',
+            ReportIntegerField('pages',
                 schemata='report',
                 lanaguageIndependent=False,
                 widget=atapi.IntegerWidget(
@@ -178,7 +136,7 @@ class SchemaExtender(PublicationSchemaExtender):
                     description=_(u'description_pages', default=u'Fill in pages'),
                 ),
             ),
-            CopyrightField('coppyrights',
+            ReportStringField('copyrights',
                 schemata='report',
                 languageIndependent=False,
                 default=COPYRIGHTS,
@@ -187,7 +145,7 @@ class SchemaExtender(PublicationSchemaExtender):
                     description=_(u'description_copyrights', default=u'Fill in copyrights'),
                 ),
             ),
-            TrailerField('trailer',
+            ReportTextField('trailer',
                 schemata='report',
                 languageIndependent=True,
                 widget=atapi.RichWidget(
@@ -204,14 +162,12 @@ class SchemaExtender(PublicationSchemaExtender):
             'reportnum',
             'series_year',
             'series_title',
-            'creators_orgs',
             'creators',
-            'publishers_orgs',
             'publishers',
             'themes',
             'price',
             'pages',
-            'coppyrights',
+            'copyrights',
             'trailer',
             'order_override_text',
             'order_extra_text',

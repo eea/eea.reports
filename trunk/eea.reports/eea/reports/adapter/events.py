3c2067cb99d8778313024964d1678ff06ed09486
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-09-25 06:50:26 +0000

    #2608   - Invalidate squid cache on file upload.

--- trunk/eea.reports/eea/reports/adapter/events.py
+++ trunk/eea.reports/eea/reports/adapter/events.py
@@ -119,6 +119,21 @@ def parse_metadata(obj, evt):
             continue
         field.getMutator(obj)(value)
 #
+# Invalidate squid cache
+#
+def invalidate_cache(instance, evt):
+    """ EVENT
+        called on new file upload. Tries to invalidate squid cache
+    """
+    stool = getToolByName(instance, 'portal_squid', None)
+    if not stool:
+        return
+    key = instance.absolute_url(1) + '/at_download/file'
+    res = stool.pruneUrls([
+        key,
+        'http/localhost/81/%s' % key
+    ])
+#
 # Report initialize
 #
 def report_initialized(obj, evt):

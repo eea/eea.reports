eac1a425444a660b811bf7423d2774c61faa21cd
Alin Voinea <contact@avoinea.com>
2011-03-09 13:58:49 +0000

    Refs #8123   - Removed portal_migation scripts as portal_migration is deprecated. Use portal_setup instead.

--- eea/reports/__init__.py
+++ eea/reports/__init__.py
@@ -7,7 +7,6 @@ from Products.Archetypes.atapi import process_types, listTypes
 from config import PROJECTNAME, DEFAULT_ADD_CONTENT_PERMISSION
 
 import patches
-import setup
 
 ppath = cmfutils.ProductsPath
 cmfutils.ProductsPath.append(dirname(package_home(globals())))
--- eea/reports/setup/__init__.py
+++
@@ -1,4 +0,0 @@
-from Products.CMFPlone import MigrationTool
-from evolve import Evolve
-
-MigrationTool.registerSetupWidget(Evolve)

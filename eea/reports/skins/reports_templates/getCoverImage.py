528fc96ce9b69f99f5babb9498292a3e3cf144c7
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2011-03-23 16:41:31 +0000

    set state for the query

--- eea/reports/skins/reports_templates/getCoverImage.py
+++ eea/reports/skins/reports_templates/getCoverImage.py
@@ -1,6 +1,8 @@
 ##parameters=REQUEST=None
 for image in context.getFolderContents(contentFilter={
-    'portal_type': 'Image',}, full_objects = True):
+    'portal_type': 'Image',
+    'review_state': ['published', 'visible'],
+}, full_objects = True):
     return image
 
 # No image found in context, try to get it from canonical

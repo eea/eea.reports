1ecab60188708b0f6854a4ea23024b2c5d2dc265
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-03-05 11:19:22 +0000

    Tagged eea.reports 4.6.

---
+++ trunk/eea.reports/tags/4.6/setup.py
@@ -0,0 +1,41 @@
+""" Installer
+"""
+from setuptools import setup, find_packages
+import os
+from os.path import join
+
+name = 'eea.reports'
+path = name.split('.') + ['version.txt']
+version = open(join(*path)).read().strip()
+
+setup(name=name,
+      version=version,
+      description="EEA Reports",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='eea reports publications',
+      author='Alin Voinea (eaudeweb), European Environment Agency',
+      author_email='webadmin@eea.europa.eu',
+      url='http://reports.eea.europa.eu',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['eea'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.ATVocabularyManager',
+          'Products.LinguaPlone',
+          'Products.AddRemoveWidget',
+          'eea.forms',
+          'eea.vocab',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

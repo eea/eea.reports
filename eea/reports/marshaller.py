464527eb71e78b82bc2539a2d7720fa551f402f3
tiberich <tiberiuichim@users.noreply.github.com>
2012-10-15 13:50:48 +0000

    Refs #7965; conform to eea.rdfmarshaller API changes

--- eea/reports/marshaller.py
+++ eea/reports/marshaller.py
@@ -1,21 +1,22 @@
 """eea.rdfmarshaller customizations for eea.reports
 """
 
+from eea.rdfmarshaller.archetypes.fields import ATField2Surf
 from eea.rdfmarshaller.interfaces import ISurfSession
-from eea.rdfmarshaller.marshaller import ATField2Surf
 from eea.reports.subtypes.field import SerialTitleField
 from zope.component import adapts
+from zope.interface import Interface
 
 
 class SerialTitle2Surf(ATField2Surf):
     """Base implementation of IATField2Surf"""
 
-    adapts(SerialTitleField, ISurfSession)
+    adapts(SerialTitleField, Interface, ISurfSession)
 
-    def value(self, context):
+    def value(self):
         """returns the value"""
 
-        v = self.field.getAccessor(context)()
+        v = self.field.getAccessor(self.context)()
         if v:
             return " - ".join(str(x).strip() for x in v if x is not None)
         else:

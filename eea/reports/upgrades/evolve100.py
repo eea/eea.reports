6eb041289971210b08591f97a3eea45d5faf1e33
Alin Voinea <contact@avoinea.com>
2019-10-03 12:15:10 +0300

    Refs #109515 - Skip Publications with no cover image generated (canonical use)

--- eea/reports/upgrades/evolve100.py
+++ eea/reports/upgrades/evolve100.py
@@ -17,12 +17,19 @@ def upgrade_cover(context):
     brains = ctool.unrestrictedSearchResults(portal_type='Report')
     to_do = len(brains)
     logger.info("Fixing %s Publication's cover image", to_do)
-    for idx, brain in enumerate(brains):
+    idx = 0
+    for brain in brains:
         doc = brain.getObject()
+        if 'cover' not in doc.objectIds():
+            continue
+
         data = StringIO(doc.file.data)
         evt = FileUploadedEvent(doc, data)
         generate_image(doc, evt)
+        
+        idx += 1
         if idx % 100 == 0:
             logger.info("Fixed %s/%s Publication's cover images", idx, to_do)
             transaction.savepoint(optimistic=True)
-    logger.info("Done fixing %s Publications's cover images", to_do)
+
+    logger.info("Done fixing %s Publications's cover images", idx)

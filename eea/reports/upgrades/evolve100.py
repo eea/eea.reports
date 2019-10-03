9b70c1b6fdd191ed80acea9779db08708f8c7893
Alin Voinea <contact@avoinea.com>
2019-10-03 16:05:57 +0300

    Refs #109515 - Regenerate all cover images

--- eea/reports/upgrades/evolve100.py
+++ eea/reports/upgrades/evolve100.py
@@ -17,18 +17,12 @@ def upgrade_cover(context):
     brains = ctool.unrestrictedSearchResults(portal_type='Report')
     to_do = len(brains)
     logger.info("Fixing %s Publication's cover image", to_do)
-    idx = 0
-    for brain in brains:
+    for idx, brain in enumerate(brains):
         doc = brain.getObject()
-        if 'cover' not in doc.objectIds():
-            continue
-
         data = StringIO(doc.file.data)
         evt = FileUploadedEvent(doc, data)
         generate_image(doc, evt)
-        
-        idx += 1
-        if idx % 100 == 0:
+        if idx % 20 == 0:
             logger.info("Fixed %s/%s Publication's cover images", idx, to_do)
             transaction.savepoint(optimistic=True)
 

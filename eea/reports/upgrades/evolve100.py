77696c7959ce06ccfa16b781b42d02ad2efa8475
Alin Voinea <contact@avoinea.com>
2019-10-09 19:50:34 +0300

    Refs #109515 - Re-generate Publications cover images asynchronous

--- eea/reports/upgrades/evolve100.py
+++ eea/reports/upgrades/evolve100.py
@@ -3,10 +3,11 @@
 import logging
 import transaction
 from cStringIO import StringIO
+from zope.component import queryUtility
 from Products.CMFCore.utils import getToolByName
 from eea.reports.events import FileUploadedEvent
 from eea.reports.adapter.events import generate_image
-
+from eea.reports.async import IAsyncService
 logger = logging.getLogger("eea.reports")
 
 
@@ -17,13 +18,31 @@ def upgrade_cover(context):
     brains = ctool.unrestrictedSearchResults(portal_type='Report')
     to_do = len(brains)
     logger.info("Fixing %s Publication's cover image", to_do)
+
+    async_service = queryUtility(IAsyncService)
     for idx, brain in enumerate(brains):
         doc = brain.getObject()
         data = StringIO(doc.file.data)
         evt = FileUploadedEvent(doc, data)
+
+        # Recreate Publications cover asynchronously via zc.async
+        if async_service:
+            async_queue = async_service.getQueues()['']
+            async_service.queueJobInQueue(
+                async_queue, ('publications',),
+                generate_image,
+                doc,
+                evt
+            )
+            continue
+
         generate_image(doc, evt)
         if idx % 20 == 0:
             logger.info("Fixed %s/%s Publication's cover images", idx, to_do)
             transaction.savepoint(optimistic=True)
 
-    logger.info("Done fixing %s Publications's cover images", idx)
+    if async_service:
+        logger.info("Fixing %s Publications's cover images schedueled. "
+                    "See async logs for more details.", idx)
+    else:
+        logger.info("Done fixing %s Publications's cover images", idx)
